c2433bff4a4d811028cfe1754e614e24cab6a642
Shobhit <shobhitsingh1149201@gmail.com>
2024-02-01 17:49:28 +0530

    dependencies and basic project structure added

---
+++ app/src/main/java/com/example/cityexplorerapp/CityExplorerApp.kt
@@ -0,0 +1,13 @@
+package com.example.cityexplorerapp
+
+import androidx.compose.runtime.Composable
+
+// TODO: Screeon enum
+
+// TODO: APP bar composable
+@Composable
+fun CityExplorerApp() {
+    // TODO: create navController and VM
+
+    // TODO: scaffold(topBar) { NavHost }
+}
